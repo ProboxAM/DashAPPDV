b682bd4d3a99ffe1a0fd134ef4db8d3f948571e3
Marc <marc.guillen.96@gmail.com>
2021-06-09 19:58:06 +0200

    devploying

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 header_y = []
 
